fe8e1780562d8d1296a0300437fb53ce4e2e5fde
TbrunoK <tbrunok@hotmail.com>
2024-09-30 18:12:46 -0300

    Correção da 2 aula

--- Origamid/javascript/javascript-completo/exercicio02/script.js
+++ Origamid/javascript/javascript-completo/exercicio02/script.js
@@ -3,8 +3,11 @@ var numero = "1"
 
 var idade = 28
 var nome = "Bruno"
-console.log(typeof nome)
-var sobreNome = "Chagas"
-var nomeCompleto = nome + sobreNome
 
+var sobreNome = "Chagas"
+var nomeCompleto = nome + " " + sobreNome
 var frase1 = "It's time"
+console.log(typeof nome)
+
+var verificarString = typeof nome
+console.log(verificarString)
