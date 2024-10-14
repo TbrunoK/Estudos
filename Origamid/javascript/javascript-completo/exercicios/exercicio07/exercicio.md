294bccde99835d636b04ccd5c784125c15a72966
TbrunoK <tbrunok@hotmail.com>
2024-10-14 06:31:52 -0300

    Exercício 07 finalizado

---
+++ Origamid/javascript/javascript-completo/exercicios/exercicio07/exercicio.md
@@ -0,0 +1,4 @@
+(X) Nomeie 3 propriedades ou métodos de strings
+(X) Nomeie 5 propriedades ou métodos de elementos do DOM
+(X) Busque na web um objeto (método) capaz de interagir com o clipboard
+// clipboard e a parte do seu computador que lida com o CTRL + C
---
+++ Origamid/javascript/javascript-completo/exercicios/exercicio07/script.js
@@ -0,0 +1,17 @@
+// Nomeie 3 propriedades ou métodos de strings
+var nome = "Bruno"
+// .fixed
+// .length
+// .slice
+
+// Nomeie 5 propriedades ou métodos de elementos do DOM
+var btn = document.querySelector(".btn")
+// addEventListener
+// appendChild
+// id
+// innerHtml
+// outerHtml
+console.log(btn)
+
+// Busque na web um objeto (método) capaz de interagir com o clipboard
+//  clipboard e a parte do seu computador que lida com o CTRL + C
