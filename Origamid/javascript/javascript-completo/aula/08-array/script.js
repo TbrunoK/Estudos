9ffb93fc9422f43391b95ec0c75b61b3040f1ecd
TbrunoK <tbrunok@hotmail.com>
2024-10-16 23:20:09 -0300

    Test

--- Origamid/javascript/javascript-completo/aula/08-array/script.js
+++ Origamid/javascript/javascript-completo/aula/08-array/script.js
@@ -2,6 +2,8 @@ var videoGames = ["Switch", "Ps4", "Xbox"]
 
 videoGames[0]
 
+console.log(videoGames[1])
+
 //  ++ = numero = numero + 1
 for (var numero = 0; numero < 10; numero++) {
   console.log(numero)
