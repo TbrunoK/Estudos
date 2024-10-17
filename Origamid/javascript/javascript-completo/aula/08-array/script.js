8312225680c89481c1aa7559d1033f2cfb41b611
TbrunoK <tbrunok@hotmail.com>
2024-10-16 23:30:54 -0300

    Praticando if else

--- Origamid/javascript/javascript-completo/aula/08-array/script.js
+++ Origamid/javascript/javascript-completo/aula/08-array/script.js
@@ -5,6 +5,17 @@ videoGames[0]
 console.log(videoGames[1])
 
 //  ++ = numero = numero + 1
-for (var numero = 0; numero < 10; numero++) {
+for (var numero = 0; numero <= 10; numero++) {
   console.log(numero)
 }
+
+// praticando
+function numeroContador(numeros) {
+  if (numeros <= 10) {
+    return console.log("É menor ou igual a dez")
+  } else {
+    return console.log("e maior que dez")
+  }
+}
+
+numeroContador(5)
