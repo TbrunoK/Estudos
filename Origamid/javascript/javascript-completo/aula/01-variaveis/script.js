2364572ad6d195ce702fea64f720b18f8befca39
TbrunoK <tbrunok@hotmail.com>
2024-10-21 12:27:25 -0300

    Aula 09 - inicio, criando layout das aulas/exercícios

--- Origamid/javascript/javascript-completo/aula/01-variaveis/script.js
+++ Origamid/javascript/javascript-completo/aula/01-variaveis/script.js
@@ -1,3 +1,16 @@
+// Referência ao elemento onde as mensagens serão mostradas
+const consoleElement = document.getElementById("console")
+
+// Armazena a referência original da função console.log
+const originalConsoleLog = console.log
+
+// Sobrescreve console.log para mostrar no console e na página
+console.log = function () {
+  originalConsoleLog.apply(console, arguments) // Chama o console.log original
+  consoleElement.innerHTML += Array.from(arguments).join(" ") + "\n" // Mostra no elemento HTML
+}
+
+// Código da aula 
 var nome = "Bruno"
 var idade = 28
 console.log(nome, idade)
