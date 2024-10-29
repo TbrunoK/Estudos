35755afb17d998da5d172891d6b3a87a7664912f
TbrunoK <tbrunok@hotmail.com>
2024-10-29 09:46:06 -0300

    Exercício 09 - finalizado

--- Origamid/javascript/javascript-completo/exercicios/exercicio09/exercicio.md
+++ Origamid/javascript/javascript-completo/exercicios/exercicio09/exercicio.md
@@ -1,10 +1,10 @@
-() Some 500 ao valor de scroll abaixo,
+(X) Some 500 ao valor de scroll abaixo,
 // Atribuindo o novo valor a scroll
 var scroll = 1000
 
 (X) Atribua true para a variável darCredito,
 // caso o cliente possua carro e casa.
 // E false caso contrario
-var possuiCarro = True 
+var possuiCarro = True
 var possuiCasa = True
 var darCredito
---
+++ Origamid/javascript/javascript-completo/exercicios/exercicio09/script.js
@@ -0,0 +1,19 @@
+// Some 500 ao valor de scroll abaixo,
+// // Atribuindo o novo valor a scroll
+// var scroll = 1000
+var scroll = 1000
+scroll += 500
+console.log(scroll)
+
+// Atribua true para a variável darCredito,
+// // caso o cliente possua carro e casa.
+// // E false caso contrario
+// var possuiCarro = True
+// var possuiCasa = True
+// var darCredito
+
+var possuiCarro = true
+var possuiCasa = true
+
+darCredito = possuiCarro && possuiCasa
+console.log(darCredito)
