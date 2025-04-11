4656fb36e92b4f8377f9f04ed882eb380e8385cc
TbrunoK <tbrunok@hotmail.com>
2025-04-11 09:36:11 -0300

    Exercício 12 Finalizado

--- Origamid/javascript/javascript-completo/exercicios/exercicio012/script.js
+++ Origamid/javascript/javascript-completo/exercicios/exercicio012/script.js
@@ -6,4 +6,18 @@ console.log(imagens)
 const imagensImagem = document.querySelectorAll('img[src^="./img/imagem"]')
 console.log(imagensImagem)
 
-//
+// Selecione todos os links internos (onde o href começa com #)
+const linksInternos = document.querySelectorAll('a[href^="#"]')
+console.log(linksInternos)
+
+// Selecione o primeiro h2 dentro de .animais-descricao
+const h2Animais = document.querySelector(".animais-descricao h2")
+console.log(h2Animais)
+
+// Selecione o último p do site
+const paragrafo = document.querySelectorAll("p")
+console.log(paragrafo[--paragrafo.length])
+
+
+
+
