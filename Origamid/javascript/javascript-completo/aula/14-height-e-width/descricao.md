615f3ccfa94e9288fb1ad6efeb8b2fb1023810ec
TbrunoK <tbrunok@hotmail.com>
2025-05-09 20:53:14 -0300

    correções

---
+++ Origamid/javascript/javascript-completo/aula/14-height-e-width/descricao.md
@@ -0,0 +1,4 @@
+# Heigth e width
+  Estas são propriedades e métodos do element e HTMLElement, a maioria delas são Read Only
+
+  
---
+++ Origamid/javascript/javascript-completo/aula/14-height-e-width/script.js
@@ -0,0 +1,5 @@
+const section = document.querySelector(".animais")
+
+section.clientHeight // heigth + padding
+section.offsetHeight // heigth + padding + border
+section.scrollHeight // heigth total, mesmo dentro do scroll
